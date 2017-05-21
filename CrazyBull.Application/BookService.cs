163d2a963a69e1d7dbada2d6080f17edca9b70d2
lynton <605653977@qq.com>
2017-05-21 22:35:05 +0800

    增加拦截器

--- CrazyBull.Application/BookService.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CrazyBull.Application
-{
-    public class Class1
-    {
-    }
-}
--- CrazyBull.Util/Validations/ValidationInterceptor.cs
+++ CrazyBull.Util/Validations/ValidationInterceptor.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using Dora.Interception;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using CrazyBull.Util.Exceptions;
 
 namespace CrazyBull.Util.Validations
 {
     public class ValidationInterceptor
     {
-
+        private InterceptDelegate _next;
+        public ValidationInterceptor(InterceptDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(InvocationContext context, IValidation validation)
+        {
+            var inputParameters = context.Method.GetParameters();
+            for (int i=0;i< inputParameters.Length; i++)
+            {
+                if (inputParameters[i].IsIn && inputParameters[i].Name.EndsWith("InputDto"))
+                {
+                    var result = validation.Validate(context.Arguments[i]);
+                    if (!result.IsValid)
+                    {
+                        throw new ValidationException(result.First().ErrorMessage);
+                    }
+                }
+            }
+            await _next(context);
+        }
     }
 }
