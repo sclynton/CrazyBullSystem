89e3fb69b98dd7071df8b39b524ac38ce5b7706c
lynton <605653977@qq.com>
2017-08-28 15:29:44 +0800

    新增SqlServer数据库仓储项目

---
+++ CrazyBull.Sqlserver.EntityFramework/CrazyBullDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CrazyBull.Core;
+using CrazyBull.Core.Articles;
+using CrazyBull.Core.Author;
+using CrazyBull.Core.Bookshelves;
+using CrazyBull.Core.Comments;
+using CrazyBull.Core.Customers;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CrazyBull.Sqlserver.EntityFramework
+{
+    public class CrazyBullDbContext : DbContext
+    {
+        public CrazyBullDbContext(DbContextOptions<CrazyBullDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Bookshelf> Bookshelves { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CrazyBull.Sqlserver.EntityFramework/Repository/BaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CrazyBull.Core;
+using Microsoft.EntityFrameworkCore;
+using CrazyBull.Sqlserver.EntityFramework;
+
+namespace CrazyBull.MySql.EntityFramework
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly CrazyBullDbContext _dbContext;
+        public Repository(CrazyBullDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public async Task<int> InsertAsync(T t)
+        {
+            _dbContext.Add(t);
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
