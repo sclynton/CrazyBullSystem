1a88133749cad1d21c8261a9de42af82d0517e49
lynton <605653977@qq.com>
2017-08-30 10:10:42 +0800

    delete bin.obj

--- CrazyBull.Entity/Bookshelves/Bookshelf.cs
+++ CrazyBull.Entity/Bookshelves/Bookshelf.cs
@@ -7,6 +7,6 @@ namespace CrazyBull.Core.Bookshelves
     public class Bookshelf : BaseEntity
     {
         public int CustomerId { get; set; }
-        public int BookId { get; set; }
+        public List<Book> Books { get; set; }
     }
 }
